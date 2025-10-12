bc2c9f198e619a91632cc51e8c2180dee06e4b9d
Shashank Vaze <155424266+shashankdv1@users.noreply.github.com>
2025-10-12 17:29:10 +0530

    Update Connect.java
    Changes and Fixes in code.

--- Cafe/Connect.java
+++ Cafe/Connect.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connect {
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/cafe";
-    private static final String USER = "root";
+    private static final String URL = "";
+    private static final String USER = "";
     private static final String PASS = "";
 
     public  Connection createConnection() {
